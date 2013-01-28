5b3d37eea257e194d988505495e8ee9653e35bee
Sergey Strukov <sshimnick@hotmail.com>
2013-01-28 21:40:57 +0400

    update

--- txt/testXXXX.cpp
+++ txt/testXXXX.cpp
@@ -1,7 +1,7 @@
 /* testXXXX..cpp */ 
 //----------------------------------------------------------------------------------------
 //
-//  Project: CCore 1.03
+//  Project: CCore 1.04
 //
 //  Tag: General HCore XCore Target/ 
 //
