5f9b67d8bbfc0ef0795a22396ffb1594e6e64f4d
Sergey Strukov <sshimnick@hotmail.com>
2013-03-30 12:12:34 +0400

    add GetPtr GetPrefix and reverse range support

--- CCore/inc/algon/BaseRangeAlgo.h
+++ CCore/inc/algon/BaseRangeAlgo.h
@@ -32,6 +32,8 @@ struct BaseRangeAlgo
  {
   typedef ulen LenType;
   
+  static auto GetPtr(R r) -> decltype(r.ptr) { return r.ptr; }
+  
   static LenType GetLen(R r) { return r.len; }
   
   static R GetPrefix(R r,LenType len) { return r.prefix(len); }
@@ -39,6 +41,8 @@ struct BaseRangeAlgo
   static R GetSuffix(R r,LenType len) { return r.suffix(len); }
   
   static R Split(R &r,LenType prefix) { return r+=prefix; }
+  
+  static R GetPrefix(R r,R suffix) { return r.prefix(suffix); }
   
   static void RangeSwap(R a,R b)
    {
@@ -51,6 +55,8 @@ struct BaseRangeAlgo<PtrLen<T> >
  {
   typedef ulen LenType;
   
+  static T * GetPtr(PtrLen<T> r) { return r.ptr; }
+  
   static LenType GetLen(PtrLen<T> r) { return r.len; }
   
   static PtrLen<T> GetPrefix(PtrLen<T> r,LenType len) { return r.prefix(len); }
@@ -59,9 +65,34 @@ struct BaseRangeAlgo<PtrLen<T> >
   
   static PtrLen<T> Split(PtrLen<T> &r,LenType prefix) { return r+=prefix; }
   
+  static PtrLen<T> GetPrefix(PtrLen<T> r,PtrLen<T> suffix) { return r.prefix(suffix); }
+  
   static void RangeSwap(PtrLen<T> a,PtrLen<T> b)
    {
     for(T *p=b.ptr; +a ;++a,++p) Swap(*a,*p); 
+   }
+ };
+
+template <class T>
+struct BaseRangeAlgo<PtrLenReverse<T> >
+ {
+  typedef ulen LenType;
+  
+  static T * GetPtr(PtrLenReverse<T> r) { return r.ptr-1; }
+  
+  static LenType GetLen(PtrLenReverse<T> r) { return r.len; }
+  
+  static PtrLenReverse<T> GetPrefix(PtrLenReverse<T> r,LenType len) { return r.prefix(len); }
+   
+  static PtrLenReverse<T> GetSuffix(PtrLenReverse<T> r,LenType len) { return r.suffix(len); }
+  
+  static PtrLenReverse<T> Split(PtrLenReverse<T> &r,LenType prefix) { return r+=prefix; }
+  
+  static PtrLenReverse<T> GetPrefix(PtrLenReverse<T> r,PtrLenReverse<T> suffix) { return r.prefix(suffix); }
+  
+  static void RangeSwap(PtrLenReverse<T> a,PtrLenReverse<T> b)
+   {
+    for(T *p=b.ptr; +a ;++a,--p) Swap(*a,p[-1]); 
    }
  };
 
