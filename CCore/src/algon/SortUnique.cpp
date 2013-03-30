2c1285733b5e885d2a7544b76507408331a3e357
Sergey Strukov <sshimnick@hotmail.com>
2013-03-30 12:13:23 +0400

    add SortUnique

---
+++ CCore/src/algon/SortUnique.cpp
@@ -0,0 +1,25 @@
+/* SortUnique.cpp */ 
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 1.04
+//
+//  Tag: General
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003 
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2013 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+ 
+#include <CCore/inc/algon/SortUnique.h>
+ 
+namespace CCore {
+namespace Algon {
+
+
+} // namespace Algon
+} // namespace CCore
+ 
+
