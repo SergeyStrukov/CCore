28c446993fe8ffa246c536697b010bfef9adb4a8
Sergey Strukov <sshimnick@hotmail.com>
2014-06-22 21:10:43 +0400

    update

--- CCore/inc/TypeSwitch.h
+++ CCore/inc/TypeSwitch.h
@@ -25,7 +25,29 @@ namespace Meta {
 
 template <class SUInt,SUInt val,class T> struct Case;
 
-template <class ... Case> struct CaseList;
+template <class ... CC> struct SwTypeOf;
+
+template <class ... CC> struct CaseList;
+
+template <class ... CC> struct SplitCaseList0;
+
+template <class ... CC> struct SplitCaseList1;
+
+template <class ... CC> struct SplitCaseList2;
+
+template <class ... CC> struct SplitCaseList3;
+
+template <class ... CC> struct SplitCaseList4;
+
+template <class ... CC> struct SplitCaseList5;
+
+template <unsigned Off,class CaseList> struct SplitCaseOf;
+
+template <class CaseList> struct CaseListExtend;
+
+template <class CaseList1,class CaseList2> struct CaseListJoin;
+
+template <unsigned Off,class ... CC> struct SplitCaseList;
 
 template <class CaseList> struct TypeSwitch;
 
@@ -34,12 +56,183 @@ template <class CaseList> struct TypeSwitch;
 template <class SUInt,SUInt val,class T> 
 struct Case
  {
+  using SwType = SUInt ;
+  
   static const SUInt Val = val ;
   
   using Type = T ;
  };
 
+/* struct SwTypeOf<CC> */
+
+template <class C,class ... CC>
+struct SwTypeOf<C,CC...>
+ {
+  using Ret = typename C::SwType ;
+ };
+
+/* struct CaseList<CC> */
+
+template <class ... CC> 
+struct CaseList
+ {
+  using SwType = typename SwTypeOf<CC...>::Ret ;
+  
+  static const unsigned Len = sizeof ... (CC) ;
+  
+  using Split = SplitCaseList<Len/2,CC...> ;
+  
+  using First = typename Split::First ;
+  using Last  = typename Split::Last ;
+ };
+
+/* struct SplitCaseList0<CC> */
+
+template <class ... CC> 
+struct SplitCaseList0
+ {
+  using First = CaseList<> ;
+  using Last = CaseList<CC...> ;
+ };
+
+/* struct SplitCaseList1<CC> */
+
+template <class C1,class ... CC> 
+struct SplitCaseList1<C1,CC...>
+ {
+  using First = CaseList<C1> ;
+  using Last = CaseList<CC...> ;
+ };
+
+/* struct SplitCaseList2<CC> */
+
+template <class C1,class C2,class ... CC> 
+struct SplitCaseList2<C1,C2,CC...>
+ {
+  using First = CaseList<C1,C2> ;
+  using Last = CaseList<CC...> ;
+ };
+
+/* struct SplitCaseList3<CC> */
+
+template <class C1,class C2,class C3,class ... CC> 
+struct SplitCaseList3<C1,C2,C3,CC...>
+ {
+  using First = CaseList<C1,C2,C3> ;
+  using Last = CaseList<CC...> ;
+ };
+
+/* struct SplitCaseList4<CC> */
+
+template <class C1,class C2,class C3,class C4,class ... CC> 
+struct SplitCaseList4<C1,C2,C3,C4,CC...>
+ {
+  using First = CaseList<C1,C2,C3,C4> ;
+  using Last = CaseList<CC...> ;
+ };
+
+/* struct SplitCaseList5<CC> */
+
+template <class C1,class C2,class C3,class C4,class C5,class ... CC> 
+struct SplitCaseList5<C1,C2,C3,C4,C5,CC...>
+ {
+  using First = CaseList<C1,C2,C3,C4,C5> ;
+  using Last = CaseList<CC...> ;
+ };
+
+/* struct SplitCaseOf<unsigned Off,CaseList> */
+
+template <unsigned Off,class ... CC>
+struct SplitCaseOf<Off,CaseList<CC...> > : SplitCaseList<Off,CC...> {};
+
+/* struct CaseListExtend<CaseList> */
+
+template <class ... CC>
+struct CaseListExtend<CaseList<CC...> >
+ {
+  template <class ... TT>
+  struct Extend
+   {
+    using Ret = CaseList<CC...,TT...> ;
+   };
+ };
+
+/* struct CaseListJoin<CaseList1,CaseList2> */
+
+template <class CaseList1,class ... CC>
+struct CaseListJoin<CaseList1,CaseList<CC...> >
+ {
+  using Extend = typename CaseListExtend<CaseList1>::template Extend<CC...> ;
+  using Ret = typename Extend::Ret ;
+ };
+
+/* struct SplitCaseList<unsigned Off,CC> */
+
+template <unsigned Off,class ... CC> 
+struct SplitCaseList
+ {
+  static const unsigned A = Off/2 ;
+  static const unsigned B = Off-A ;
+  
+  using SplitA = SplitCaseList<A,CC...> ;
+  
+  using SplitB = SplitCaseOf<B,typename SplitA::Last> ;
+  
+  using First = typename CaseListJoin<typename SplitA::First,typename SplitB::First>::Ret ;
+  using Last = typename SplitB::Last ;
+ };
+
+template <class ... CC> 
+struct SplitCaseList<0,CC...> : SplitCaseList0<CC...> {};
+
+template <class ... CC> 
+struct SplitCaseList<1,CC...> : SplitCaseList1<CC...> {};
+
+template <class ... CC> 
+struct SplitCaseList<2,CC...> : SplitCaseList2<CC...> {};
+
+template <class ... CC> 
+struct SplitCaseList<3,CC...> : SplitCaseList3<CC...> {};
+
+template <class ... CC> 
+struct SplitCaseList<4,CC...> : SplitCaseList4<CC...> {};
+
+template <class ... CC> 
+struct SplitCaseList<5,CC...> : SplitCaseList5<CC...> {};
+
 /* struct TypeSwitch<CaseList> */
+
+template <class CaseList> 
+struct TypeSwitch
+ {
+  using SwType = typename CaseList::SwType ;
+  using First = typename CaseList::First ;
+  using Last = typename CaseList::Last ;
+  
+  template <class Ctx,class RetType_>
+  struct ContCtx
+   {
+    using RetType = RetType_ ;
+    
+    Ctx &ctx;
+    
+    explicit ContCtx(Ctx &ctx_) : ctx(ctx_) {}
+    
+    template <class T>
+    RetType call() { return ctx.template call<T>(); }
+    
+    RetType defcall(SwType val)
+     {
+      return TypeSwitch<Last>::template Switch<Ctx &,RetType>(val,ctx);
+     }
+   };
+  
+  template <class Ctx,class RetType=typename Ctx::RetType>
+  static RetType Switch(SwType val,Ctx ctx)
+   {
+    return TypeSwitch<First>::Switch(val,ContCtx<Ctx,RetType>(ctx));
+   }
+ };
 
 template <class SUInt,SUInt val1,class T1>
 struct TypeSwitch<CaseList<Case<SUInt,val1,T1> > >
--- CCore/test/test0092.TypeSwitch.cpp
+++ CCore/test/test0092.TypeSwitch.cpp
@@ -15,10 +15,157 @@
 
 #include <CCore/test/test.h>
 
+#include <CCore/inc/TypeSwitch.h>
+
 namespace App {
 
 namespace Private_0092 {
 
+template <unsigned Ind> 
+struct Node
+ {
+  template <class P>
+  static void Print(P &out)
+   {
+    Printf(out,"Node<#;>\n",Ind);
+   }
+ };
+
+template <class ... CC> struct PrintList;
+
+template <class C,class ... CC> 
+struct PrintList<C,CC...>
+ {
+  template <class P>
+  static void Print(P &out)
+   {
+    C::Type::Print(out);
+    
+    PrintList<CC...>::Print(out);
+   }
+ };
+
+template <> 
+struct PrintList<>
+ {
+  template <class P>
+  static void Print(P &)
+   {
+   }
+ };
+
+template <class CaseList> struct PrintCaseList;
+
+template <class ... CC>
+struct PrintCaseList<Meta::CaseList<CC...> >
+ {
+  template <class P>
+  static void Print(P &out)
+   {
+    PrintList<CC...>::Print(out);
+    
+    Putch(out,'\n');
+   }
+ };
+
+template <class CaseList,ulen Len>
+struct PrintSplit
+ {
+  template <class P>
+  static void Print(P &out)
+   {
+    PrintSplit<CaseList,Len-1>::Print(out);
+    
+    Printf(out,"-----\n\n");
+    
+    PrintCaseList<typename Meta::SplitCaseOf<Len,CaseList>::First>::Print(out);
+    
+    Putch(out,'\n');
+    
+    PrintCaseList<typename Meta::SplitCaseOf<Len,CaseList>::Last>::Print(out);
+   }
+ };
+
+template <class CaseList>
+struct PrintSplit<CaseList,0>
+ {
+  template <class P>
+  static void Print(P &out)
+   {
+    PrintCaseList<typename Meta::SplitCaseOf<0,CaseList>::First>::Print(out);
+    
+    Putch(out,'\n');
+    
+    PrintCaseList<typename Meta::SplitCaseOf<0,CaseList>::Last>::Print(out);
+   }
+ };
+
+template <unsigned Len>
+struct MakeCaseList
+ {
+  using Ret = typename Meta::CaseListJoin<typename MakeCaseList<Len-1>::Ret,Meta::CaseList<Meta::Case<unsigned,Len,Node<Len> > > >::Ret ;
+  
+  static void test1()
+   {
+    PrintCon out(Con);
+    
+    PrintCaseList<Ret>::Print(out);
+   }
+  
+  static void test2()
+   {
+    PrintCon out(Con);
+    
+    PrintSplit<Ret,Len>::Print(out);
+   }
+
+  struct Ctx
+   {
+    using RetType = void ;
+    
+    template <class T>
+    static void call()
+     {
+      PrintCon out(Con);
+      
+      T::Print(out);
+     }
+    
+    static void defcall(unsigned sw)
+     {
+      Printf(Con,"default #;\n",sw);
+     }
+   };
+  
+  static void test3()
+   {
+    PrintCon out(Con);
+    
+    for(unsigned sw=0; sw<=Len ;sw++)
+      Meta::TypeSwitch<Ret>::Switch(sw,Ctx());
+   }
+ };
+
+template <>
+struct MakeCaseList<0>
+ {
+  using Ret = Meta::CaseList<> ;
+ };
+
+void test1()
+ {
+  MakeCaseList<100>::test1();  
+ }
+
+void test2()
+ {
+  MakeCaseList<10>::test2();  
+ }
+
+void test3()
+ {
+  MakeCaseList<100>::test3();
+ }
 
 } // namespace Private_0092
  
@@ -32,6 +179,10 @@ const char *const Testit<92>::Name="Test92 TypeSwitch";
 template<>
 bool Testit<92>::Main() 
  {
+  //test1();
+  //test2();
+  test3();
+  
   return true;
  }
  
